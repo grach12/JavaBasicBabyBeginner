5742d64aec0cb262284b81d7c35a3a5b0be9c39d
grach12 <60659966+grach12@users.noreply.github.com>
2020-02-04 22:48:50 +0200

    Add files via upload

---
+++ SimpleOperation/RectangSurface.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class RectangSurface {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        var x1 = Double.parseDouble(scan.nextLine());
+        var y1 = Double.parseDouble(scan.nextLine());
+        var x2 = Double.parseDouble(scan.nextLine());
+        var y2 = Double.parseDouble(scan.nextLine());
+
+        double lenght = Math.abs(x1-x2);
+        double width = Math.abs (y1-y2);
+
+        double area = lenght*width;
+        double perimtr = 2*(lenght+width);
+
+        System.out.printf("%.2f%n",area);
+        System.out.printf("%.2f", perimtr);
+
+
+
+    }
+
+}
+
+
