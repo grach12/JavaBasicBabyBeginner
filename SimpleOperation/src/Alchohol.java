475447339a554a5a7c595dc5e044ced5112d7459
User <radoslav_radev@hotmail.com>
2020-02-07 15:11:09 +0200

    alkohol

--- SimpleOperation/src/Alchohol.java
+++ SimpleOperation/src/Alchohol.java
@@ -10,6 +10,11 @@ public class Alchohol {
         var whiskeyLiters = Double.parseDouble(scan.nextLine());
 
         var rakaiaPrrice = priceWhiskey/2;
+        var whinePrice = rakaiaPrrice-rakaiaPrrice*0.4;
+        var beerPrice = rakaiaPrrice-rakaiaPrrice*0.8;
+
+        var rakiaSum = rakiaLiters*rakaiaPrrice;
+        var beerSum = beerLiters*beerPrice;
 
         System.out.println(rakaiaPrrice);
 
