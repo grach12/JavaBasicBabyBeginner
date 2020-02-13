5a1349806e289c6f100e391451b2c8e72e427b2b
User <radoslav_radev@hotmail.com>
2020-02-13 15:24:24 +0200

    Обръщение според възраст и пол

--- NestedConditionalStatment/src/Gendar.java
+++ NestedConditionalStatment/src/Gendar.java
@@ -4,23 +4,29 @@ public class Gendar {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         var age = Double.parseDouble(scan.nextLine());
-        String gendar = scan.nextLine();
+        String gender = scan.nextLine();
 
-        if (gendar.equals("m")) {
+        if (gender.equals("m")) {
             if (age >= 16) {
                 System.out.println("Mr.");
-        }
+            }
             else {
             System.out.println("Master");
+            }
         }
-    }  else if (gendar.equals("f")) {
+
+        else if (gender.equals("f")) {
             if (age >= 16) {
                 System.out.println("Ms.");
             } else {
                 System.out.println("Miss");
             }
-
         }
+
+        //else {
+        //    System.out.println("unknown");
+        //}
+
     }
 }
 
