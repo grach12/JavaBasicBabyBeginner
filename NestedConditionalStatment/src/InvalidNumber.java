0dcfb3853fdb0af12178b851cc83025f5ba08c79
User <radoslav_radev@hotmail.com>
2020-02-14 15:43:07 +0200

    Невалидно число

---
+++ NestedConditionalStatment/src/InvalidNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class InvalidNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        var number = Integer.parseInt(scan.nextLine());
+ if (number == 0) {
+     System.exit(1); //After this command java stops immediately!
+ }
+else if ((number<100) || (number>200)) {
+            System.out.println("invalid number");
+        }
+    }
+}
