c476b7b75d5226d32f9d078267f381e61f7d9c66
grach12 <60659966+grach12@users.noreply.github.com>
2020-02-15 22:18:27 +0200

    15 февр. неделя до Ски ваканция final

--- NestedConditionalStatment/src/SkiVacation.java
+++ NestedConditionalStatment/src/SkiVacation.java
@@ -8,20 +8,119 @@ public class SkiVacation {
         String roomType = scan.nextLine();
         String feedback = scan.nextLine();
 
-        double price = 0.00;
+        double price;
         var totalPrice = 0.00;
+        var finalPrice = 0.00;
 
-        switch (roomType) {
-            case "room for one person":
-                price = 18.00;
-                break;
-            case "apartment":
-                price = 25.00;
-                break;
-            case "president apartment":
-                price = 35.00;
-                break;
-            default:System.out.printf("%.2f",totalPrice+(totalPrice*0.25)); }
+
+        if ("room for one person".equals(roomType)) {
+            price = 18.00;
+            if (feedback.equals("positive")){
+                totalPrice=price*(days-1);
+                finalPrice=totalPrice+(totalPrice*0.25);
+                System.out.printf("%.2f",finalPrice);
+            }
+            else if (feedback.equals("negative")){
+                totalPrice=price*(days-1);
+                finalPrice=totalPrice-(totalPrice*0.10);
+                System.out.printf("%.2f",finalPrice);
+            }
+        }
+        else if ("apartment".equals(roomType)&&(days<10)){
+            price = 25.00;
+            if (feedback.equals("positive")){
+                totalPrice= (price - price * 0.30)*(days-1);
+                finalPrice=totalPrice+(totalPrice*0.25);
+                System.out.printf("%.2f",finalPrice);
+            }
+            else if (feedback.equals("negative")){
+                totalPrice= (price - price * 0.30)*(days-1);
+                finalPrice=totalPrice-(totalPrice*0.10);
+                System.out.printf("%.2f",finalPrice);
+            }
+
+        }
+        else if ("apartment".equals(roomType)&&((days>10)&&(days<15))){
+            price = 25.00;
+            if (feedback.equals("positive")){
+                totalPrice= (price - price * 0.35)*(days-1);
+                finalPrice=totalPrice+(totalPrice*0.25);
+                System.out.printf("%.2f",finalPrice);
+            }
+            else if (feedback.equals("negative")) {
+                totalPrice = (price - price * 0.35) * (days - 1);
+                finalPrice = totalPrice - (totalPrice * 0.10);
+                System.out.printf("%.2f", finalPrice);
+            }
+        }
+        else if ("apartment".equals(roomType)&&((days>15))){
+            price = 25.00;
+            if (feedback.equals("positive")){
+                totalPrice= (price - price * 0.50)*(days-1);
+                finalPrice=totalPrice+(totalPrice*0.25);
+                System.out.printf("%.2f",finalPrice);
+            }
+            else if (feedback.equals("negative")) {
+                totalPrice = (price - price * 0.50) * (days - 1);
+                finalPrice = totalPrice - (totalPrice * 0.10);
+                System.out.printf("%.2f", finalPrice);
+            }
+        }
+
+
+
+        else if ("president apartment".equals(roomType)&&(days<10)){
+            price = 35.00;
+            if (feedback.equals("positive")){
+                totalPrice= (price - price * 0.10)*(days-1);
+                finalPrice=totalPrice+(totalPrice*0.25);
+                System.out.printf("%.2f",finalPrice);
+            }
+            else if (feedback.equals("negative")){
+                totalPrice= (price - price * 0.10)*(days-1);
+                finalPrice=totalPrice-(totalPrice*0.10);
+                System.out.printf("%.2f",finalPrice);
+            }
+
+        }
+        else if ("president apartment".equals(roomType)&&((days>10)&&(days<15))){
+            price = 35.00;
+            if (feedback.equals("positive")){
+                totalPrice= (price - price * 0.15)*(days-1);
+                finalPrice=totalPrice+(totalPrice*0.25);
+                System.out.printf("%.2f",finalPrice);
+            }
+            else if (feedback.equals("negative")) {
+                totalPrice = (price - price * 0.15) * (days - 1);
+                finalPrice = totalPrice - (totalPrice * 0.10);
+                System.out.printf("%.2f", finalPrice);
+            }
+        }
+        else if ("president apartment".equals(roomType)&&((days>15))){
+            price = 35.00;
+            if (feedback.equals("positive")){
+                totalPrice= (price - price * 0.20)*(days-1);
+                finalPrice=totalPrice+(totalPrice*0.25);
+                System.out.printf("%.2f",finalPrice);
+            }
+            else if (feedback.equals("negative")) {
+                totalPrice = (price - price * 0.20) * (days - 1);
+                finalPrice = totalPrice - (totalPrice * 0.10);
+                System.out.printf("%.2f", finalPrice);
+            }
+        }
+
+
+
+    }
+}
+      /*  } else if ("apartment".equals(roomType)) {
+            price = 25.00;
+        } else if ("president apartment".equals(roomType)) {
+            price = 35.00;
+        } else {
+            System.out.printf("%.2f", totalPrice + (totalPrice * 0.25));
+        }
         if (feedback.equals("positive")){
 
         if ((days < 10) && (roomType.equals("apartment"))) {
@@ -60,7 +159,7 @@ public class SkiVacation {
 
 
 
-    /*    else {
+      else {
 
         }
         //System.out.println(totalPrice);
