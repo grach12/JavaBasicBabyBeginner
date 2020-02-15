0bacbf285f51aec2196e435180cd2cd058b590ef
grach12 <60659966+grach12@users.noreply.github.com>
2020-02-15 11:40:39 +0200

    Магазин за плодове

--- NestedConditionalStatment/src/FruitShop.java
+++ NestedConditionalStatment/src/FruitShop.java
@@ -7,67 +7,68 @@ public class FruitShop {
         String fruit = scan.nextLine();
         String dayOfWeek = scan.nextLine();
         var count = Double.parseDouble(scan.nextLine());
-var bananaPrice = 2.50; var applePrice = 1.20; var orangePrice = 0.85; var grapefruitPrice = 1.45;
-var kiwiPrice = 2.70; var pineapplePrice = 5.50; var grapesPrice = 3.85;
+        // var bananaPrice = 2.50; var applePrice = 1.20; var orangePrice = 0.85; var grapefruitPrice = 1.45;
+        // var kiwiPrice = 2.70; var pineapplePrice = 5.50; var grapesPrice = 3.85;
 
-        if ((dayOfWeek.equals("Saturday"))||(dayOfWeek.equals("Sunday"))) {
+        if (dayOfWeek.equals("Monday") || dayOfWeek.equals("Tuesday") || dayOfWeek.equals("Wednesday")
+                || dayOfWeek.equals("Thursday") || dayOfWeek.equals("Friday")) {
             switch (fruit) {
                 case "banana":
-                    System.out.println(count * 2.70);
+                    System.out.printf("%.2f", count * 2.50);
                     break;
                 case "apple":
-                    System.out.println(count * 1.25);
+                    System.out.printf("%.2f", count * 1.20);
                     break;
                 case "orange":
-                    System.out.println(count * 0.90);
+                    System.out.printf("%.2f", count * 0.85);
                     break;
                 case "grapefruit":
-                    System.out.println(count * 1.60);
+                    System.out.printf("%.2f", count * 1.45);
                     break;
                 case "kiwi":
-                    System.out.println(count * 3.00);
+                    System.out.printf("%.2f", count * 2.70);
                     break;
                 case "pineapple":
-                    System.out.println(count * 5.60);
+                    System.out.printf("%.2f", count * 5.50);
                     break;
                 case "grapes":
-                    System.out.println(count * 4.20);
+                    System.out.printf("%.2f", count * 3.85);
                     break;
                 default:
                     System.out.println("error");
             }
         }
-        else if ((dayOfWeek.equals("Monday")) || (dayOfWeek.equals("Tuesday "))|| (dayOfWeek.equals("Wednesday "))
-        || (dayOfWeek.equals("Thursday")) || (dayOfWeek.equals("Friday")))
-        {
-                switch (fruit) {
-                    case "banana":
-                        System.out.println(count * bananaPrice);
-                        break;
-                    case "apple":
-                        System.out.println(count * applePrice);
-                        break;
-                    case "orange":
-                        System.out.println(count * orangePrice);
-                        break;
-                    case "grapefruit":
-                        System.out.println(count * grapefruitPrice);
-                        break;
-                    case "kiwi":
-                        System.out.println(count * kiwiPrice);
-                        break;
-                    case "pineapple":
-                        System.out.println(count * pineapplePrice);
-                        break;
-                    case "grapes":
-                        System.out.println(count * grapesPrice);
-                        break;
-                    default:
-                        System.out.println("error");
-                }
-            } else {
-                System.out.println("error");
+        else if ( (dayOfWeek.equals("Saturday") || (dayOfWeek.equals("Sunday")) )) {
+            switch (fruit) {
+                case "banana":
+                    System.out.printf("%.2f",count * 2.70);
+                    break;
+                case "apple":
+                    System.out.printf("%.2f",count * 1.25);
+                    break;
+                case "orange":
+                    System.out.printf("%.2f",count * 0.90);
+                    break;
+                case "grapefruit":
+                    System.out.printf("%.2f",count * 1.60);
+                    break;
+                case "kiwi":
+                    System.out.printf("%.2f",count * 3.00);
+                    break;
+                case "pineapple":
+                    System.out.printf("%.2f",count * 5.60);
+                    break;
+                case "grapes":
+                    System.out.printf("%.2f",count * 4.20);
+                    break;
+                default:
+                    System.out.println("error");
             }
+        }
+
+        else{
+            System.out.println("error");
+        }
 
 
 
