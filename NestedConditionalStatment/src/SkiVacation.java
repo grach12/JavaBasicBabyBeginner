b6ad8dd9e7269b97eee65be6f94dd4c960026848
grach12 <60659966+grach12@users.noreply.github.com>
2020-02-15 14:12:10 +0200

    15 февр. неделя до Ски ваканция

---
+++ NestedConditionalStatment/src/SkiVacation.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SkiVacation {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        var number = Integer.parseInt(scan.nextLine());
+        String product = scan.nextLine();
+        String feedback = scan.nextLine();
+
+    }
+}
