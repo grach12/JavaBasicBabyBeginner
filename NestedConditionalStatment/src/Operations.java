18947541334383bf90f563c213650c9977b64185
grach12 <60659966+grach12@users.noreply.github.com>
2020-02-18 22:07:09 +0200

    Операции между числа

--- NestedConditionalStatment/src/Operations.java
+++ NestedConditionalStatment/src/Operations.java
@@ -4,17 +4,57 @@ public class Operations {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        var N1 = Integer.parseInt(scan.nextLine());
-        var N2 = Integer.parseInt(scan.nextLine());
+        var N1 = Double.parseDouble(scan.nextLine());
+        var N2 = Double.parseDouble(scan.nextLine());
+        var plus = N1+N2;
+        var minus = N1-N2;
+        var multiply = N1*N2;
         String operator = scan.nextLine();
 
         switch (operator){
-            case "+":
-                System.out.printf("%d + %d = %d",N1, N2, N1+N2);
+            case "+": if (plus%2==0) {
+                System.out.printf("%.0f + %.0f = %.0f - even", N1, N2, N1 + N2);
+            }
+            else{
+                System.out.printf("%.0f + %.0f = %.0f - odd", N1, N2, N1 + N2);
+            }
                 break;
-            case "-":
-                System.out.printf("%d + %d = %d",N1, N2, N1+N2);
+            case "-": if (minus%2==0) {
+                System.out.printf("%.0f - %.0f = %.0f - even", N1, N2, N1 - N2);
+            }
+            else {
+                System.out.printf("%.0f - %.0f = %.0f - odd", N1, N2, N1 - N2);
+            }
+            break;
+            case "*": if (multiply%2==0){
+                System.out.printf("%.0f * %.0f = %.0f - even",N1, N2, N1*N2);
+            }
+            else {
+                System.out.printf("%.0f * %.0f = %.0f - odd",N1, N2, N1*N2);
+            }
                 break;
+            case "/": if (N2!=0) {
+                System.out.printf("%.0f / %.0f = %.2f", N1, N2, N1 / N2);
+            }
+            else {
+                System.out.printf("Cannot divide %.0f by zero", N1);
+            }
+            break;
+            case "%": if (N2!=0) {
+                System.out.printf("%.0f %% %.0f = %.0f", N1, N2, N1 % N2);
+            }
+            else {
+                System.out.printf("Cannot divide %.0f by zero", N1);
+            }
+            break;
+
+           /* default: if (operator.equals("/")) {
+                System.out.printf("Cannot divide %d by zero", N1);
+            }
+            if (operator.equals("%")) {
+                System.out.printf("Cannot divide %d by zero",N1);
+            }
+*/
 
         }
     }
