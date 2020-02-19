04149a6f634ed59a9ab7c6e38b36ff6652cc41b9
Sector <radoslav_radev@hotmail.com>
2020-02-19 22:31:50 +0200

    ХотелскаСтая - РАботииии :)))

--- NestedConditionalStatment/src/HotelRoom.java
+++ NestedConditionalStatment/src/HotelRoom.java
@@ -5,11 +5,10 @@ public class HotelRoom {
         Scanner scan = new Scanner(System.in);
 
         String month = scan.nextLine();
-        var nights = Integer.parseInt(scan.nextLine());
-
+        var nights = Double.parseDouble(scan.nextLine());
         var studioPrice = 0.0;
         var apartmentPrice = 0.0;
-
+        var totalprice=0.0;
 
         switch (month) {
             case "May":
@@ -18,45 +17,50 @@ public class HotelRoom {
                 apartmentPrice = 65;
 
                 if (nights > 14) {
-                    apartmentPrice = (apartmentPrice - apartmentPrice * 0.1) * nights;
-                    System.out.printf("Apartment: %.2f lv.%n",apartmentPrice);
+                    totalprice = (apartmentPrice - apartmentPrice * 0.1) * nights;
+                    System.out.printf("Apartment: %.2f lv.%n",totalprice);
                 } else {
                     System.out.printf("Apartment: %.2f lv.%n",(apartmentPrice * nights));
                 }
 
                 if ((nights > 7) && (nights < 14)) {
-                    studioPrice = (studioPrice - studioPrice * 0.05) * nights;
-                    System.out.printf("Studio: %.2f lv.",studioPrice);
+                    totalprice = (studioPrice - studioPrice * 0.05) * nights;
+                    System.out.printf("Studio: %.2f lv.", totalprice);
                 } else if (nights > 14) {
-                    studioPrice = (studioPrice - studioPrice * 0.30) * nights;
-                    System.out.printf("Studio: %.2f lv.",studioPrice);
+                    totalprice = (studioPrice - studioPrice * 0.30) * nights;
+                    System.out.printf("Studio: %.2f lv.", totalprice);
+                }
+                else {
+                    System.out.printf("Studio: %.2f lv.", studioPrice*nights);
                 }
                 break;
+
             case "June":
             case "September":
                 studioPrice = 75.20;
                 apartmentPrice = 68.70;
                 if (nights > 14) {
-                    apartmentPrice = (apartmentPrice - apartmentPrice * 0.1) * nights;
-                    System.out.printf("Apartment: %.2f lv.%n",apartmentPrice);
+                    totalprice = (apartmentPrice - apartmentPrice * 0.1) * nights;
+                    System.out.printf("Apartment: %.2f lv.%n", totalprice);
                 } else {
                     System.out.printf("Apartment: %.2f lv.%n", (apartmentPrice * nights));
                 }
 
                 if (nights > 14) {
-                    studioPrice = (studioPrice - studioPrice * 0.20) * nights;
-                    System.out.printf("Studio: %.2f lv.", studioPrice);
+                    totalprice = (studioPrice - studioPrice * 0.20) * nights;
+                    System.out.printf("Studio: %.2f lv.", totalprice);
                 } else {
                     System.out.printf("Studio: %.2f lv.", studioPrice * nights);
                 }
                 break;
+
             case "July":
             case "August":
                 studioPrice = 76.00;
                 apartmentPrice = 77.00;
                 if (nights > 14) {
-                    apartmentPrice = (apartmentPrice - apartmentPrice * 0.1) * nights;
-                    System.out.printf("Apartment: %.2f lv.%n", apartmentPrice);
+                    totalprice = (apartmentPrice - apartmentPrice * 0.10)*nights;
+                    System.out.printf("Apartment: %.2f lv.%n", totalprice);
                 } else {
                     System.out.printf("Apartment: %.2f lv.%n",(apartmentPrice * nights));
                 }
@@ -64,7 +68,7 @@ public class HotelRoom {
                 System.out.printf("Studio: %.2f lv.", (studioPrice * nights));
                 break;
         }
-        }
-
     }
+
+}
 
