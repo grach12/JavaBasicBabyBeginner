1a07f7c0479d4fa44b2768f818a67b86b545dade
grach12 <60659966+grach12@users.noreply.github.com>
2020-02-22 00:11:39 +0200

    На време за изпит

--- NestedConditionalStatment/src/OnTimeForExam.java
+++ NestedConditionalStatment/src/OnTimeForExam.java
@@ -1,9 +1,9 @@
+
 import java.util.Scanner;
 
 public class OnTimeForExam {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        var squareMeters = Double.parseDouble(scan.nextLine());
 
         var hoursExam = Integer.parseInt(scan.nextLine())*60;
         var minutesExam = Integer.parseInt(scan.nextLine());
@@ -12,10 +12,79 @@ public class OnTimeForExam {
 
         var timeOfExam = hoursExam+minutesExam;
         var arrivalTime = hoursIn+minutesIn;
-        //до тук всичко е в минути, надявам се :)
+        //до тук всичко е в минути
+
+        if(arrivalTime>timeOfExam){
+            System.out.println("Late");
+        }
+        else if ((timeOfExam==arrivalTime)||(arrivalTime>=timeOfExam-30)) {
+            System.out.println("On time");
+        }
+        else if ((arrivalTime)<(timeOfExam-30)){
+            System.out.println("Early");
+        }
 
 
+        if ((arrivalTime>timeOfExam-60)&&(arrivalTime< timeOfExam)){
+            System.out.printf("%d minutes before the start", (timeOfExam-arrivalTime));
+        }
+        else if (arrivalTime<=timeOfExam-60){
 
+            System.out.printf("%d:%02d hours before the start", ((timeOfExam-arrivalTime)/60), ((timeOfExam-arrivalTime)%60));
+        }
+        else if ((arrivalTime>timeOfExam)&&(arrivalTime < timeOfExam+60)){
+            System.out.printf("%d minutes after the start", (arrivalTime-timeOfExam));
+        }
+
+        else if (arrivalTime>=timeOfExam+60){
+            System.out.printf("%d:%02d hours after the start", ((arrivalTime-timeOfExam)/60), ((arrivalTime-timeOfExam)%60));
+        }//  %02d  форматира int 2 знака след запетаята
 
     }
 }
+
+
+/*import java.util.Scanner;
+
+public class OnTimeForExam {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        var hoursExam = Double.parseDouble(scan.nextLine())*60;
+        var minutesExam = Double.parseDouble(scan.nextLine());
+        var hoursIn = Double.parseDouble(scan.nextLine())*60;
+        var minutesIn = Double.parseDouble(scan.nextLine());
+
+        var timeOfExam = hoursExam+minutesExam;
+        var arrivalTime = hoursIn+minutesIn;
+        //до тук всичко е в минути, надявам се :)
+
+        if(arrivalTime>timeOfExam){
+            System.out.println("Late");
+        }
+else if ((timeOfExam==arrivalTime)||(arrivalTime>=timeOfExam-30)) {
+    System.out.println("On time");
+}
+else if ((arrivalTime)<(timeOfExam-30)){
+            System.out.println("Early");
+        }
+
+
+if ((arrivalTime>timeOfExam-60)&&(arrivalTime< timeOfExam)){
+    System.out.printf("%.0f minutes before the start", (timeOfExam-arrivalTime));
+}
+else if (arrivalTime<=timeOfExam-60){
+
+   System.out.printf("%.0f:%.0f hours before the start", Math.floor((timeOfExam-arrivalTime)/60), ((timeOfExam-arrivalTime)%60));
+}
+else if ((arrivalTime>timeOfExam)&&(arrivalTime < timeOfExam+60)){
+    System.out.printf("%.0f minutes after the start", (arrivalTime-timeOfExam));
+}
+
+else if (arrivalTime>timeOfExam+60){
+    System.out.printf("%.0f:%.0f hours after the start",Math.floor((arrivalTime-timeOfExam)/60), ((arrivalTime-timeOfExam)%60));
+}
+
+    }
+}
+*/
