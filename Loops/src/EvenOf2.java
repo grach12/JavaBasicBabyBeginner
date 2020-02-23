008d93f6a9f2dc146895b89849bfce6b577ef83c
Sector <radoslav_radev@hotmail.com>
2020-02-23 18:08:08 +0200

    Четни степени на 2

--- Loops/src/EvenOf2.java
+++ Loops/src/EvenOf2.java
@@ -4,9 +4,10 @@ public class EvenOf2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = Integer.parseInt(scan.nextLine());
-
-        for (int i=n; i<=n%2; i++){
-            System.out.println(i);
+        int num = 1;
+        for (int i=0; i<=n; i+=2){
+            System.out.println(num);
+            num=num*2*2;
         }
 
     }
