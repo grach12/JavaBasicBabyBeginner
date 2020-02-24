00ee13eda44167ab95b3cf1773fbcb908c383491
User <radoslav_radev@hotmail.com>
2020-02-24 11:13:04 +0200

    числа до 1000, завършващи на 7 комбинираме for цикъл за преминаваме през числата от 1 до 1000 и проверка за всяко число дали завършва на 7.

---
+++ Loops/src/Numberto1000endby7.java
@@ -0,0 +1,11 @@
+public class Numberto1000endby7 {
+    public static void main(String[] args) {
+      //  Scanner scan = new Scanner(System.in);
+
+        for (int i = 1 ; i < 1000 ; i++) {
+            if (i % 10 == 7) {
+                System.out.println(i);
+            }
+        }
+        }
+    }
