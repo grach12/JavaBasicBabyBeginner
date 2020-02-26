4e557424a7818d894a88c31cf67718d8b9fa9920
Sector <radoslav_radev@hotmail.com>
2020-02-26 23:56:51 +0200

    Цикли Началото

---
+++ Loops/src/DayOfTheWeek.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class DayOfTheWeek {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        for (int day = 1; day <= 7; day++) {
+            System.out.println("Day number: " +day);
+        }
+
+    }
+}
