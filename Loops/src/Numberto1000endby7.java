f720a8ea3c58905a70e3f0feaf532c7318740831
grach12 <60659966+grach12@users.noreply.github.com>
2020-02-27 16:11:08 +0200

    Update Numberto1000endby7.java
    Числата до 7 решено без деление с остатък i%10==7

--- Loops/src/Numberto1000endby7.java
+++ Loops/src/Numberto1000endby7.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
+
+//public class Numberto1000endby7 {
+//    public static void main(String[] args) {
+//      //  Scanner scan = new Scanner(System.in);
+//
+//        for (int i = 7 ; i <= 997 ; i++) {
+//            if (i % 10 == 7) {
+//                System.out.println(i);
+//            }
+//        }
+//        }
+//    }
 public class Numberto1000endby7 {
     public static void main(String[] args) {
-      //  Scanner scan = new Scanner(System.in);
-
-        for (int i = 1 ; i < 1000 ; i++) {
-            if (i % 10 == 7) {
-                System.out.println(i);
-            }
-        }
+        Scanner scan = new Scanner(System.in);
+        for (int i = 7; i <=997 ; i+=10) {
+            System.out.println(i);
         }
     }
+}
