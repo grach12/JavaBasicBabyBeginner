012ba89b62141bcf56ea496102fe4fe8cc97c540
Sector <radoslav_radev@hotmail.com>
2020-03-02 20:46:21 +0200

    Умната Лили

--- Loops/src/SmartLili.java
+++ Loops/src/SmartLili.java
@@ -5,8 +5,31 @@ public class SmartLili {
 
         Scanner scan = new Scanner(System.in);
 
-        var liily = Integer.parseInt(scan.nextLine());
-
+        var liilyAge = Integer.parseInt(scan.nextLine());
+        double priceWashing = Double.parseDouble(scan.nextLine());
+        int priceToy = Integer.parseInt(scan.nextLine());
+int toyCount=0;
+double liliMoney = 0.0;
+int moneyForBDay = 10;
+        for (int i = 1; i <=liilyAge ; i++) {
+            if (i %2==0){ // проверяваме дали възрастта на Лили(i) е четна
+             liliMoney=moneyForBDay+liliMoney-1; // искам да разбера не може и по този начин  liliMoney = liliMoney+(i+10); и вадим 1 лев , който взима брат и
+                moneyForBDay= moneyForBDay+10;  // подготваме парите за рожден ден за следваща стъпка
+            }
+            else {
+                toyCount++; // това е същото като toyCount = toyCount+1;
+            }
+        }
+        double moneyFromTOys = toyCount*priceToy; // можем ли да го инициализираме в началото ?
+        liliMoney = liliMoney+moneyFromTOys;
+        if (liliMoney>=priceWashing){
+            double moneyLeft = liliMoney-priceWashing;
+            System.out.printf("Yes! %.2f", moneyLeft);
+        }
+        else {
+            double moneyNeed = priceWashing-liliMoney;
+            System.out.printf("No! %.2f", moneyNeed);
+        }
     }
 }
 
