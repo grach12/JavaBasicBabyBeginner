40f58c5ec93d9be2d7cfccfb17767db8f1cfc6ec
Sector <radoslav_radev@hotmail.com>
2020-03-04 23:54:57 +0200

    Четна Нечетна Сума - работи , работи и в Judge exercize loop зад.3, трябва да се махне Double.MAX_VALUE и да се замени с 1000000000.0

--- Loops/src/OddEvenPosition.java
+++ Loops/src/OddEvenPosition.java
@@ -5,42 +5,135 @@ public class OddEvenPosition {
         Scanner scan = new Scanner(System.in);
 
         int n = Integer.parseInt(scan.nextLine());
-        //double sum = 0;
-       // double min = Double.MAX_VALUE;
-       //double max = Double.MIN_VALUE;
-        double oddSum=0;
-        double oddMin=Double.MAX_VALUE;
-        double oddMax= Double.MIN_VALUE;
-        double evenSum=0;
-        double evenMin=Double.MAX_VALUE;
-        double evenMax=Double.MIN_VALUE;
 
-        for (int i = 0; i <n ; i++) {
+        double oddSum = 0;
+        double oddMin = 1000000000.0;
+        double oddMax = -1000000000.0;
+        double evenSum = 0;
+        double evenMin = 1000000000.0;
+        double evenMax = -1000000000.0;
+
+        for (int i = 1; i <= n; i++) {
             double number = Double.parseDouble(scan.nextLine());
             if (i % 2 == 0) {
-                oddSum = number+oddSum;  //evenSum=evenSum+number;
-
-            if (number>oddMax) {
-                oddMax = number;
-            }else if (number<oddMin){
-                oddMin = number;
-            }
-        }
-            else {
-                evenSum=number+evenSum;
-                if(number>evenMin){
-                    evenMax=number;
-                }else if (number<evenMin){
-                    evenMin=number;
+                evenSum = number + evenSum;  //evenSum=evenSum+number;
+                if (evenMax < number) {
+                    evenMax = number;
                 }
+                if (evenMin > number) {
+                    evenMin = number;
+                }
+            } else {
+                oddSum = number + oddSum;
+                if (oddMax < number) {
+                    oddMax = number;
+                }
+                if (oddMin > number) {
+                    oddMin = number;
+                }
+
             }
         }
-        System.out.println(oddSum);
-        System.out.println(oddMin);
-        System.out.println(oddMax);
-        System.out.println(evenSum);
-        System.out.println(evenMin);
-        System.out.println(evenMax);
+        System.out.printf("OddSum=%.2f,%n", oddSum);
+
+        if (oddMin >= 1000000000.0) {
+            System.out.println("OddMin=No,");
+        } else {
+            System.out.printf("OddMin=%.2f,%n", oddMin);
+        }
+        if (oddMax <= -1000000000.0) {
+            System.out.println("OddMax=No,");
+        } else {
+            System.out.printf("OddMax=%.2f,%n", oddMax);
+        }
+
+        System.out.printf("EvenSum=%.2f,%n", evenSum);
+
+
+        if (evenMin >= 1000000000.0) {
+            System.out.println("EvenMin=No,");
+        } else {
+            System.out.printf("EvenMin=%.2f,%n", evenMin);
+        }
+        if (evenMax <= -1000000000.0) {
+            System.out.println("EvenMax=No");
+        } else {
+            System.out.printf("EvenMax=%.2f", evenMax);
+        }
+
 
     }
 }
+
+
+
+//Отдолу е моето неРешение
+/*import java.util.Scanner;
+
+public class OddEvenPosition {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+
+        double evenSum = 0;
+        double evenMin = Double.MAX_VALUE;
+        double evenMax = Double.MIN_VALUE;
+        double oddSum = 0;
+        double oddMin = Double.MAX_VALUE;
+        double oddMax = Double.MIN_VALUE;
+
+        for (int i = 1; i <= n; i++) {
+            double number = Double.parseDouble(scan.nextLine());
+            if (i % 2 == 0) {
+                evenSum = number + evenSum;  //evenSum=evenSum+number;
+                if (evenMax < number) {
+                    evenMax = number;
+                }
+                if (evenMin > number) {
+                    evenMin = number;
+                }
+            } else {
+                oddSum = number + oddSum;
+                if (oddMax < number) {
+                    oddMax = number;
+                }
+                if (oddMin > number) {
+                    oddMin = number;
+                }
+
+            }
+        }
+            System.out.printf("OddSum=%.2f,%n", oddSum);
+
+            if (oddMin > Integer.MAX_VALUE) {
+                System.out.println("OddMin=No,");
+            } else {
+                System.out.printf("OddMin=%.2f,%n", oddMin);
+            }
+            if (oddMax < Integer.MIN_VALUE) {
+                System.out.println("OddMax=No,");
+            } else {
+                System.out.printf("OddMax=%.2f,%n", oddMax);
+            }
+
+            System.out.printf("EvenSum=%.2f%n", evenSum);
+
+
+            if (evenMin > Integer.MAX_VALUE) {
+                System.out.println("EvenMin=No,");
+            } else {
+                System.out.printf("EvenMin=%.2f,%n", evenMin);
+            }
+            if (evenMax < Integer.MIN_VALUE) {
+                System.out.println("EvenMax=No,");
+            } else {
+                System.out.printf("EvenMax=%.2f", evenMax);
+            }
+
+
+    }
+}
+
+
+*/
