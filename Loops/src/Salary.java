6a688bb5c3941c04129700be78a4270a8fdab0ae
Sector <radoslav_radev@hotmail.com>
2020-03-05 23:26:02 +0200

    Заплата

---
+++ Loops/src/Salary.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Salary {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+
+        double salary = Double.parseDouble(scan.nextLine());
+
+        String a = "Facebook";
+        String b = "Instagram";
+        String c = "Reddit";
+
+
+
+    }
+}
