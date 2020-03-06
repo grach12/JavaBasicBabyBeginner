d80346b7d87f114b6918c4d37eb5887d417e1db6
Rado <radoslav_radev@hotmail.com>
2020-03-06 15:03:06 +0200

    Повторения с цикли – while-цикъл Loops2

---
+++ Loops2/src/ReadingWords.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ReadingWords {
+    public static void main(String[] args) {
+          Scanner scan = new Scanner(System.in);
+          String name = scan.nextLine();
+while (!name.equals("Stop")){
+    name = scan.nextLine();
+}
+    }
+}
+       /* int a=5;
+        while (a<=10){
+            System.out.println(a);
+            a++;
+        }
+    }
+}
+*/
