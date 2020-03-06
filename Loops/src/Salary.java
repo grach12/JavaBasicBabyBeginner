5c83a1c743ee7d982e25fc85e4b5f8a995826316
Rado <radoslav_radev@hotmail.com>
2020-03-06 13:44:52 +0200

    Заплата, вярна е , но в Judge ми дава 88% https://judge.softuni.bg/Contests/Compete/Index/1164#5

--- Loops/src/Salary.java
+++ Loops/src/Salary.java
@@ -5,14 +5,29 @@ public class Salary {
         Scanner scan = new Scanner(System.in);
 
         int n = Integer.parseInt(scan.nextLine());
-
         double salary = Double.parseDouble(scan.nextLine());
 
-        String a = "Facebook";
-        String b = "Instagram";
-        String c = "Reddit";
+        //String a = "Facebook";
+        //String b = "Instagram";
+        //String c = "Reddit";
 
+        for (int i = 1; i <=n ; i++) {
+            String name = scan.nextLine();
 
+            if (name.equals("Facebook")) {
+                salary = salary - 150;
+            }
+            if (name.equals("Instagram")) {
+                salary = salary - 100;
+            }
+            if (name.equals("Reddit")) {
+                salary = salary - 50;
+            }
+            else if (salary<=0){
+                System.out.println("You have lost your salary.");
+            return;} // с return излизам от кода
 
+        }
+        System.out.printf("%.0f",salary);
     }
 }
