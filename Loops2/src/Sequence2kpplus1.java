507950ae98264c5c370ed39fdd8141802aa87746
Sector <radoslav_radev@hotmail.com>
2020-03-06 20:44:58 +0200

    Редица числа 2k+1

---
+++ Loops2/src/Sequence2kpplus1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Sequence2kpplus1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+
+        int num = 1;
+
+        while (num<=n){
+            System.out.println(num);
+            num = num*2+1;
+        }
+
+    }
+}
