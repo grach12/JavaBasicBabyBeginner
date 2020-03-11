b5df44557e07ee03700363678368578c9f1cd7a7
radoslav_radev@hotmail.com <Ardiop12>
2020-03-11 16:27:09 +0200

    Торта

---
+++ Loops2/src/Cake.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Cake {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int width = Integer.parseInt(scan.nextLine());
+        int lenght = Integer.parseInt(scan.nextLine());
+
+        int pieceCake = width*lenght;
+        String input = scan.nextLine(); // същото ли е като "";
+
+        while (!input.equals("STOP")){  //същото ли е като input=="Stop"
+            int piecesTaken = Integer.parseInt(input);
+            pieceCake-=piecesTaken;
+
+            if (pieceCake<0){
+                break;
+            }
+            input = scan.nextLine();
+        }
+
+if (pieceCake>=0){
+    System.out.printf("%d pieces are left.", pieceCake);
+}
+else{
+    System.out.printf("No more cake left! You need %d pieces more.", Math.abs(pieceCake));
+}
+
+    }
+}
