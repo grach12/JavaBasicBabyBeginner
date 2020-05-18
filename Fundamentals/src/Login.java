d2f58d216d6b702af76df24f57b5105457dc061c
Rado@Work <radoslav_radev@hotmail.com>
2020-05-18 14:13:25 +0300

    5.	Login You will be given a string representing a username. The password will be that username reversed. Until you receive the correct password print on the console "Incorrect password. Try again.". When you receive the correct password print "User {username} logged in." However on the fourth try if the password is still not correct print "User {username} blocked!" and end the program. Examples Input	Output Acer login go let me in recA 	Incorrect password. Try again. Incorrect password. Try again. Incorrect password. Try again. User Acer logged in.

---
+++ Fundamentals/src/Login.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class Login {
+
+        public static void main(String[] args) {
+
+            Scanner scan =new Scanner(System.in);
+
+            String userName = scan.nextLine();
+            int userLenght = userName.length();
+
+            for (int i =userLenght-1 ; i >=0 ; i--) {
+                System.out.print(userName.charAt(i));
+                String pass = "";
+            }
+
+
+//
+//            while (){
+//
+//            }
+
+        }
+    }
