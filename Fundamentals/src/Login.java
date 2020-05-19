0389384997196198ec3be0582129d47b2606ce68
Rado@Work <radoslav_radev@hotmail.com>
2020-05-19 11:30:43 +0300

    Java Fundamentals - EXERCISE: BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS 5 Login

--- Fundamentals/src/Login.java
+++ Fundamentals/src/Login.java
@@ -3,23 +3,32 @@ import java.util.Scanner;
 
 public class Login {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            Scanner scan =new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-            String userName = scan.nextLine();
-            int userLenght = userName.length();
-            String pass = "";
-            for (int i =userLenght-1 ; i >=0 ; i--) {
-                pass = pass+userName.charAt(i);
-                //String.format("%s%s", pass, userName.charAt(i));
+        String userName = scan.nextLine();
+        String oldUserName = userName;
+        int userLenght = userName.length();
+        String pass = "";
+
+        for (int i = userLenght - 1; i >= 0; i--) {
+            pass = pass + userName.charAt(i);
+            //String.format("%s%s", pass, userName.charAt(i));
+        }
+        int counter = 0;
+        userName = scan.nextLine();
+        while (!userName.equals(pass)) {
+            counter++;
+            if (counter == 4) {
+                System.out.printf("User %s blocked!", oldUserName);
+                break;
             }
-            System.out.println(pass);
-
-//
-//            while (){
-//
-//            }
-
+            System.out.println("Incorrect password. Try again.");
+            userName = scan.nextLine();
+        }
+        if (counter < 4) {
+            System.out.printf("User %s logged in.", oldUserName);
         }
     }
+}
